6b82f1ad70b550d2b73d33f3abc64ab5640ca464
sultik <sultan.yerbulatov@gmail.com>
2022-04-06 13:49:10 +0500

    End Of Section 2 - API

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/DataModels/Address.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StudentAdminPortal.API.DataModels
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string PhysicalAddress { get; set; }
+        public string PostalAddress { get; set; }
+
+        //Navigation propery
+        public Guid StudentId { get; set; }
+    }
+}
